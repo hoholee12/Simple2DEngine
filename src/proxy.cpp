8f4ef1cce59809cd307c00b0cd002b2372597740
hoholee12 <hoholee12@gmail.com>
2023-11-19 21:19:33 +0900

    base

---
+++ src/proxy.cpp
@@ -0,0 +1,120 @@
+#include "proxy.h"
+
+class proxy{
+public:
+    // audio stuff
+    SDL_AudioDeviceID audio_device;
+    SDL_AudioSpec gonna, have;
+    // SDL stuff
+    SDL_Renderer* renderer;
+    SDL_Window* window;
+    SDL_Texture* texture;
+    SDL_Rect* rect;
+    // opengl stuff
+    unsigned int VBO, VAO, EBO;
+    mutable float vertices[4290];
+    int shaderProgram;
+    ImVec4 clear_color = ImVec4(0.0f, 0.0f, 0.0f, 1.00f);
+
+    int screenWidth, screenHeight;
+    const char* windowstr = "test";
+    void videoInit(){
+
+        screenWidth = 1280;
+        screenHeight = 720;
+
+        SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER);
+
+        // GL 3.0 + GLSL 130
+        const char* glsl_version = "#version 130";
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+
+        // Create window with graphics context
+        SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+        SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+        SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+        SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI);
+        
+        window = SDL_CreateWindow(windowstr, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screenWidth, screenHeight, window_flags);
+        SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+        SDL_GL_MakeCurrent(window, gl_context);
+        SDL_GL_SetSwapInterval(1); // Enable vsync
+
+        gladLoadGL();
+
+
+        //vertex shader
+        const char *vertexShaderSource = "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0";
+        int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+        glCompileShader(vertexShader);
+
+        //fragment shader
+        const char *fragmentShaderSource = "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "   FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
+        "}\n\0";
+        int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+        glCompileShader(fragmentShader);
+
+        //link them together
+        shaderProgram = glCreateProgram();
+        glAttachShader(shaderProgram, vertexShader);
+        glAttachShader(shaderProgram, fragmentShader);
+        glLinkProgram(shaderProgram);
+        //dont need this anymore
+        glDeleteShader(vertexShader);
+        glDeleteShader(fragmentShader);
+
+       //int indicesLength = convertVideoToIndices(tempBuffer, indices, screenHeight, screenWidth);
+
+        //generate EBO, VAO, VBO
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+        glGenBuffers(1, &EBO);
+
+        // 1. bind Vertex Array Object
+        glBindVertexArray(VAO);
+        // 2. copy our vertices array in a vertex buffer for OpenGL to use
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+        // 3. copy our index array in a element buffer for OpenGL to use
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * indicesLength, indices, GL_DYNAMIC_DRAW);
+        // 4. then set the vertex attributes pointers
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+    }
+
+    void drawVideo(){
+        glViewport(0, 0, screenWidth, screenHeight);
+        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //video render
+        //int indicesLength = convertVideoToIndices(videoBuffer, indices, screenHeight, screenWidth);
+        // 3. copy our index array in a element buffer for OpenGL to use
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * indicesLength, indices, GL_DYNAMIC_DRAW);
+
+        //run shader
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        //glDrawElements(GL_TRIANGLES, indicesLength, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
+        //sync
+        SDL_GL_SwapWindow(window);
+    }
+};
--- src/proxy.h
+++ src/proxy.h
@@ -6,4 +6,41 @@
 *
 */
 
+#ifdef __linux__
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+#include <sys/mman.h>
+#include<dirent.h>
+#endif
+
+#ifdef _WIN32
+#include <windows.h>
+#ifdef __cplusplus
+extern "C"
+#endif
+#endif
+
+#pragma warning(disable:4018; disable:4996; disable:4244)
+
+#ifdef _WIN32
+#include<SDL.h>
+#else
+#include<SDL2/SDL.h>
+#endif
+
+
+#include<cstdlib>
+#include<cstdio>
+#include<cstring>
+#include<string>
+#include<cstdint>
+using namespace std;
+
+
+#include "imgui.h"
+#include "imgui_impl_sdl.h"
+#include "imgui_impl_opengl3.h"
+#include <glad/glad.h>
+
 
